beb2f2a0914bda1cadae2da9ae50611815522e63
Uroš Jurglič <jurglic@antiveb.com>
2010-10-14 23:15:43 +0200

    Handler optimization

--- lib/jbuilder/jbuilder_handler.rb
+++ lib/jbuilder/jbuilder_handler.rb
@@ -2,13 +2,7 @@ module JBuilder
   
   class JBuilderHandler < ActionView::Template::Handler
     def self.call(template)
-      result  = "json = JBuilder::JBuilderDsl.new;\n"
-      result += "self.class.send :define_method, :object, Proc.new {|*args, &code| json.object(*args, &code)};\n"
-      result += "self.class.send :define_method, :array, Proc.new {|*args, &code| json.array(*args, &code)};\n"
-      result += "self.class.send :define_method, :value, Proc.new {|*args| json.value(*args)};\n"
-      result += "#{template.source};\n"
-      result += "json.output;"
-      result
+      "json = JBuilder::JBuilderDsl.new;\n#{template.source};\njson.output;"
     end
   end
 
